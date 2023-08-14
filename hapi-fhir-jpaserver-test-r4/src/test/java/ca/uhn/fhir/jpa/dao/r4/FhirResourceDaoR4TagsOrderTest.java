77f75cd34d26206d254d5d7129a785e4eb5804e8
Emre Dincturk <emre.dincturk@smilecdr.com>
2023-08-14 16:42:45 -0400

    eod

--- hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4TagsOrderTest.java
+++ hapi-fhir-jpaserver-test-r4/src/test/java/ca/uhn/fhir/jpa/dao/r4/FhirResourceDaoR4TagsOrderTest.java
@@ -4,13 +4,16 @@ import ca.uhn.fhir.jpa.api.config.JpaStorageSettings;
 import ca.uhn.fhir.jpa.api.model.DaoMethodOutcome;
 import ca.uhn.fhir.jpa.test.BaseJpaR4Test;
 import ca.uhn.fhir.storage.TagOrderInterceptor;
+import graphql.Assert;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.CanonicalType;
 import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.IdType;
 import org.hl7.fhir.r4.model.Meta;
 import org.hl7.fhir.r4.model.Patient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -22,7 +25,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FhirResourceDaoR4TagsOrderTest extends BaseJpaR4Test {
-
 
 
 	private TagOrderInterceptor myTagOrderInterceptor;
@@ -41,45 +43,185 @@ public class FhirResourceDaoR4TagsOrderTest extends BaseJpaR4Test {
 		myInterceptorRegistry.unregisterInterceptor(myTagOrderInterceptor);
 	}
 
+/*	@Test
+	public void testWhenResourceCreated_TagsOrderedOnRead() {
+		JpaStorageSettings.TagStorageModeEnum tagStorageMode = JpaStorageSettings.TagStorageModeEnum.INLINE;*/
 	@ParameterizedTest
 	@EnumSource(JpaStorageSettings.TagStorageModeEnum.class)
-	public void testTagsOrderedOnCreate(JpaStorageSettings.TagStorageModeEnum tagStorageMode) {
+	public void testWhenResourceCreated_TagsOrderedOnRead(JpaStorageSettings.TagStorageModeEnum tagStorageMode) {
 		myStorageSettings.setTagStorageMode(tagStorageMode);
 
-		DaoMethodOutcome createOutcome = createPatientWithUnorderedInitialTags();
-
-		Patient createdPatient = myPatientDao.read(createOutcome.getResource().getIdElement(), mySrd);
-
-		List<Coding> expectedCodingList = List.of(
-			createCoding("sys1", "A"),
-			createCoding("sys1", "B"),
-			createCoding("sys1", "C"),
-			createCoding("sys2", "A"),
-			createCoding("sys2", "B"),
-			createCoding("sys2", "C")
+		Meta metaInputOnCreate = createMeta(
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //tag
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //security
+			List.of("c", "b", "a") // profile
 		);
 
-		verifyCodingsInOrder(expectedCodingList, createdPatient.getMeta().getTag());
-		verifyCodingsInOrder(expectedCodingList, createdPatient.getMeta().getSecurity());
-		assertEquals(List.of("A", "B", "C"), toStringList(createdPatient.getMeta().getProfile()));
+
+		DaoMethodOutcome createOutcome = createPatient(metaInputOnCreate);
+
+		IIdType patientId = createOutcome.getResource().getIdElement();
+		Patient createdPatient = myPatientDao.read(patientId, mySrd);
+
+
+		Meta expectedMetaAfterCreate = createMeta(
+			generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "b", "c")), //tag (sorted)
+			generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "b", "c")), //security (sorted)
+			List.of("a", "b", "c") //profile (sorted)
+		);
+
+
+		verifyCodingsInOrder(expectedMetaAfterCreate.getTag(), createdPatient.getMeta().getTag());
+		verifyCodingsInOrder(expectedMetaAfterCreate.getSecurity(), createdPatient.getMeta().getSecurity());
+		assertEquals(toStringList(expectedMetaAfterCreate.getProfile()), toStringList(createdPatient.getMeta().getProfile()));
+
+
+		// FIX ME: The meta endpoint isn't calling PRESHOW, so ordering is not working
+		// TODO: $meta operation doesn't return tags when using INLINE storage mode https://github.com/hapifhir/hapi-fhir/issues/5206
+/*		Meta meta = myPatientDao.metaGetOperation(Meta.class, patientId, mySrd);
+		verifyCodingsInOrder(expectedCodingList, meta.getTag());
+		verifyCodingsInOrder(expectedCodingList, meta.getSecurity());
+		assertEquals(List.of("A", "B", "C"), toStringList(meta.getProfile()));*/
+
 	}
+
 	@ParameterizedTest
 	@EnumSource(JpaStorageSettings.TagStorageModeEnum.class)
 	public void testUpdateShouldNotIncreaseVersionBecauseOfTagOrder(JpaStorageSettings.TagStorageModeEnum tagStorageMode) {
 		myStorageSettings.setTagStorageMode(tagStorageMode);
 
-		DaoMethodOutcome createOutcome = createPatientWithUnorderedInitialTags();
+		Meta metaInputOnCreate = createMeta(
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //tag
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //security
+			List.of("c", "b", "a") // profile
+		);
 
-		DaoMethodOutcome updateOutcome = updatePatientWithUnorderedInitialTags(createOutcome.getResource().getIdElement());
+
+		DaoMethodOutcome createOutcome = createPatient(metaInputOnCreate);
+		IIdType patientId = createOutcome.getId();
+
+		// use the same input on update as the create
+		Meta metaInputOnUpdate = createMeta(
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //tag
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //security
+			List.of("c", "b", "a") // profile
+		);
+		// updating the resource with the same set of tags shouldn't cause a new version to be created
+		DaoMethodOutcome updateOutcome = updatePatient(patientId, metaInputOnUpdate);
 
 		assertTrue(updateOutcome.isNop());
 
 		Patient patient = myPatientDao.read(createOutcome.getResource().getIdElement(), mySrd);
 		assertEquals("1", patient.getMeta().getVersionId());
+
 	}
 
-	private DaoMethodOutcome updatePatientWithUnorderedInitialTags(IIdType patientId) {
-		Meta meta = constructTestMetaWithUnorderedInitialTags();
+
+
+	@ParameterizedTest
+	@EnumSource(
+		value = JpaStorageSettings.TagStorageModeEnum.class,
+		names = {"INLINE"},
+		mode = EnumSource.Mode.EXCLUDE)
+	public void testWhenResourceUpdate_TagsOrderedOnRead_NonInlineModes(JpaStorageSettings.TagStorageModeEnum tagStorageMode) {
+		myStorageSettings.setTagStorageMode(tagStorageMode);
+			// meta input for initial create
+		Meta metaInputOnCreate = createMeta(
+				generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //tag
+				generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //security
+				List.of("c", "b", "a") // profile
+			);
+			//meta expected on read after create (everything must be sorted):
+			Meta expectedMetaAfterCreate =  createMeta(
+				generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "b", "c")), //tag (sorted)
+				generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "b", "c")), //security (sorted)
+				List.of("a", "b", "c") //profile (sorted)
+			);
+			// meta input for update (adding new tags)
+		Meta metaInputOnUpdate = createMeta(
+				generateAllCodingPairs(List.of("sys2", "sys1"), List.of("cc", "bb", "aa")), //tag
+				generateAllCodingPairs(List.of("sys2", "sys1"), List.of("cc", "bb", "aa")), //security
+				List.of("cc", "bb", "aa") //profile
+			);
+			// the new tags must be added to the existing set and must appear in the right order
+		Meta expectedMetaAfterUpdate = createMeta(
+			generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "aa", "b", "bb", "c", "cc")), //tag (sorted)
+			generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "aa", "b", "bb", "c", "cc")), //security (sorted)
+			List.of("aa", "bb", "cc") //profile (replaced and sorted)
+			);
+
+		testCreateAndUpdate(metaInputOnCreate, expectedMetaAfterCreate, metaInputOnUpdate, expectedMetaAfterUpdate);
+
+	}
+
+
+
+	@Test
+	public void testWhenResourceUpdate_TagsOrderedOnRead_InlineMode() {
+		myStorageSettings.setTagStorageMode(JpaStorageSettings.TagStorageModeEnum.INLINE);
+
+		Meta metaInputOnCreate = createMeta(
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //tag
+			generateAllCodingPairs(List.of("sys2", "sys1"), List.of("c", "b", "a")), //security
+			List.of("c", "b", "a") // profile
+		);
+		//meta expected on read after create (everything must be sorted):
+		Meta expectedMetaAfterCreate =  createMeta(
+				generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "b", "c")), //tag (sorted)
+				generateAllCodingPairs(List.of("sys1", "sys2"), List.of("a", "b", "c")), //security (sorted)
+				List.of("a", "b", "c") //profile (sorted)
+			);
+
+		// meta input for update (adding new tags)
+		Meta metaInputOnUpdate = createMeta(
+				generateAllCodingPairs(List.of("sys2", "sys1"), List.of("cc", "bb", "aa")), //tag
+				generateAllCodingPairs(List.of("sys2", "sys1"), List.of("cc", "bb", "aa")), //security
+				List.of("cc", "bb", "aa") //profile
+			);
+
+		// the new tags must be added to the existing set and must appear in the right order
+		Meta expectedMetaAfterUpdate = createMeta(
+				generateAllCodingPairs(List.of("sys1", "sys2"), List.of("aa", "bb", "cc")), //tag (sorted)
+				generateAllCodingPairs(List.of("sys1", "sys2"), List.of("aa", "bb", "cc")), //security (sorted)
+				List.of("aa", "bb", "cc") //profile (sorted)
+			);
+
+		testCreateAndUpdate(metaInputOnCreate, expectedMetaAfterCreate, metaInputOnUpdate, expectedMetaAfterUpdate);
+
+	}
+
+		public void testCreateAndUpdate(Meta metaInputOnCreate, Meta expectedMetaAfterCreate, Meta metaInputOnUpdate, Meta expectedMetaAfterUpdate) {
+
+		IIdType patientId = new IdType().setValue("patientId1");
+		DaoMethodOutcome updateOutcome = updatePatient(patientId, metaInputOnCreate);
+		Assert.assertFalse(updateOutcome.isNop());
+
+		Patient patient = myPatientDao.read(patientId, mySrd);
+		assertEquals("1", patient.getMeta().getVersionId());
+
+		verifyCodingsInOrder(expectedMetaAfterCreate.getTag(), patient.getMeta().getTag());
+		verifyCodingsInOrder(expectedMetaAfterCreate.getSecurity(), patient.getMeta().getSecurity());
+		assertEquals(toStringList(expectedMetaAfterCreate.getProfile()), toStringList(patient.getMeta().getProfile()));
+
+		updateOutcome = updatePatient(patientId, metaInputOnUpdate);
+		Assert.assertFalse(updateOutcome.isNop());
+
+		patient = myPatientDao.read(patientId, mySrd);
+		assertEquals("2", patient.getMeta().getVersionId());
+
+		verifyCodingsInOrder(expectedMetaAfterUpdate.getTag(), patient.getMeta().getTag());
+		verifyCodingsInOrder(expectedMetaAfterUpdate.getSecurity(), patient.getMeta().getSecurity());
+		assertEquals(toStringList(expectedMetaAfterUpdate.getProfile()), toStringList(patient.getMeta().getProfile()));
+
+		// FIX ME: The meta endpoint isn't calling PRESHOW, so ordering is not working
+/*		Meta meta = myPatientDao.metaGetOperation(Meta.class, patientId, mySrd);
+		verifyCodingsInOrder(expectedCodingList, meta.getTag());
+		verifyCodingsInOrder(expectedCodingList, meta.getSecurity());
+		assertEquals(List.of("A", "B", "C"), toStringList(meta.getProfile()));*/
+
+	}
+
+	private DaoMethodOutcome updatePatient(IIdType patientId, Meta meta) {
 		Patient inputPatient = new Patient();
 		inputPatient.setId(patientId);
 		inputPatient.setMeta(meta);
@@ -87,39 +229,26 @@ public class FhirResourceDaoR4TagsOrderTest extends BaseJpaR4Test {
 		return myPatientDao.update(inputPatient, mySrd);
 	}
 
-	private DaoMethodOutcome createPatientWithUnorderedInitialTags() {
-		Meta meta = constructTestMetaWithUnorderedInitialTags();
-		Patient inputPatient = new Patient();
-		inputPatient.setId("p");
-		inputPatient.setMeta(meta);
 
+	private DaoMethodOutcome createPatient(Meta meta) {
+		Patient inputPatient = new Patient();
+		inputPatient.setMeta(meta);
 		return myPatientDao.create(inputPatient, mySrd);
 	}
 
-	private Meta constructTestMetaWithUnorderedInitialTags( ) {
 
-		List<Coding> tags = List.of(
-			createCoding("sys2", "C"),
-			createCoding("sys2", "B"),
-			createCoding("sys2", "A"),
-			createCoding("sys1", "C"),
-			createCoding("sys1", "B"),
-			createCoding("sys1", "A")
-		);
 
-		List<Coding>  securityTags = List.of(
-			createCoding("sys2", "C"),
-			createCoding("sys2", "B"),
-			createCoding("sys2", "A"),
-			createCoding("sys1", "C"),
-			createCoding("sys1", "B"),
-			createCoding("sys1", "A")
-		);
-
-		List<String> profiles = List.of("C", "B", "A");
-
-		return createMeta(tags, securityTags, profiles);
+	private static List<Coding> generateAllCodingPairs(List<String> systems, List<String> codes) {
+		List<Coding> result = new ArrayList<>();
+		for (String system: systems) {
+			for (String code: codes) {
+				result.add(createCoding(system, code));
+			}
+		}
+		return result;
 	}
+
+
 
 	private void verifyCodingsInOrder(List<Coding> expectedCodings, List<Coding> actualCodings) {
 		assertEquals(expectedCodings.size(), actualCodings.size());
@@ -134,11 +263,11 @@ public class FhirResourceDaoR4TagsOrderTest extends BaseJpaR4Test {
 		return canonicalTypes.stream().map(c -> c.getValue()).collect(Collectors.toList());
 	}
 
-	private Coding createCoding(String system, String code) {
+	private static Coding createCoding(String system, String code) {
 		return new Coding().setSystem(system).setCode(code);
 	}
 
-	private Meta createMeta(List<Coding> tags, List<Coding> securityLabels, List<String> profiles){
+	private static Meta createMeta(List<Coding> tags, List<Coding> securityLabels, List<String> profiles) {
 		Meta meta = new Meta();
 		meta.setTag(new ArrayList<>(tags));
 		meta.setSecurity(new ArrayList<>(securityLabels));
